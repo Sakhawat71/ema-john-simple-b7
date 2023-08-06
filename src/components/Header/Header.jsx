0bf10dc0c325b962916f58f901ac1dfa7071ccd4
sakhawat71 <sakhawat.showrav@gmail.com>
2023-08-07 00:14:05 +0600

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import './Header.css';
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav className="header">
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div className="link">
+                <a href="/order">Order</a>
+                <a href="/review">Order Review</a>
+                <a href="/manage">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
