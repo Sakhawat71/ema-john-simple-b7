3e732260dd1ebd9547e98b37279382c8150f2d97
sakhawat71 <sakhawat.showrav@gmail.com>
2023-08-22 19:40:28 +0600

    addtoCart_use_simpleWay

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,19 +8,22 @@ import './Cart.css'
 
 
 const Cart = ({cart}) => {
-    // console.log(cart);
+
     let total = 0;
     let shipping = 0;
     let grandTotal = 0;
-    // let quantity = 0;
+    let quantity = 0;
+
     for(let product of cart){
 
+        product.quantity = product.quantity || 1;
 
-
-        total = total + product.price;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
-        // quantity = quantity + product.quantity;
+          
+        quantity = quantity + product.quantity;
     }
+
     let tax = total * 7 /100;
     grandTotal = grandTotal + total + tax;
 
@@ -28,7 +31,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h2 style={{marginLeft:'15px'}}>Order Summary</h2>
-                <p>Selected Items: {cart.length}</p>
+                <p>Selected Items: {quantity}</p>
                 <p>Total Price: ${total}</p>
                 <p>Total Shipping Charge: ${shipping}</p>
                 <p>Tax: ${parseFloat(tax.toFixed(2))}</p>
